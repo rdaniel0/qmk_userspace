be7fb6b1fea68b15fc39b34b60151eece69d6645
rdaniel0 <rablin.daniel@gmail.com>
2025-05-21 22:18:05 +1000

    feat: alternate repeat macros
    Add macros for reducing SFBs in graphite
    Moved some definitions to .h file

---
+++ keyboards/cheapino/keymaps/dan/rules.mk
@@ -0,0 +1,9 @@
+CAPS_WORD_ENABLE = yes
+CUSTOM_MATRIX = lite
+WS2812_DRIVER = vendor
+RGBLIGHT_ENABLE = yes
+DEFERRED_EXEC_ENABLE = yes
+SRC += encoder.c
+SRC += ghosting.c
+SRC += matrix.c
+REPEAT_KEY_ENABLE = yes
